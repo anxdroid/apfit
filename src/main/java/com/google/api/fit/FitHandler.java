45026107a83ff517102267690df267c4c1da4775
Antonello <paoletti.antonello@gmail.com>
2017-09-04 19:52:31 +0200

    modifiche

---
+++ src/main/java/com/google/api/fit/FitHandler.java
@@ -0,0 +1,10 @@
+package com.google.api.fit;
+
+import com.fasterxml.jackson.core.JsonFactory;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.services.fitness.Fitness;
+import com.google.api.services.fitness.model.*;
+
+public class FitHandler {
+
+}
